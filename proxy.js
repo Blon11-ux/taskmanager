50955542e04df9ec40503bbc0a9eb82007ce0ad9
Blon Tamang Bipin <251019@st.yoshida-g.ac.jp>
2026-05-25 11:11:28 +0900

    proxy

--- proxy.js
+++ proxy.js
@@ -1,18 +1,15 @@
-// middleware.js
 import { NextResponse } from "next/server";
 
-export function middleware(request) {
+export function proxy(request) {
   const sessionCookie = request.cookies.get("session_user_id");
   const isLoggedIn = !!sessionCookie?.value;
 
   const { pathname } = request.nextUrl;
 
-  // If logged in and trying to visit /auth, redirect home
   if (isLoggedIn && pathname.startsWith("/auth")) {
     return NextResponse.redirect(new URL("/", request.url));
   }
 
-  // If not logged in and trying to visit /, redirect to /auth
   if (!isLoggedIn && pathname === "/") {
     return NextResponse.redirect(new URL("/auth", request.url));
   }
@@ -20,7 +17,6 @@ export function middleware(request) {
   return NextResponse.next();
 }
 
-// Tell Next.js which routes this middleware runs on
 export const config = {
   matcher: ["/", "/auth"],
 };
